53739942ce2c1050a6318d9ff5dd09d457c2eb69
Takuya Kishira <takuya.kishira@drecom.co.jp>
2020-05-24 18:54:05 +0900

    update deploy setting

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xe
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+cd $SCRIPT_DIR
+
 /usr/bin/git pull origin master
 
 /usr/local/bin/docker-compose build
+
+/usr/local/bin/docker-compose stop
 
 nohup /usr/local/bin/docker-compose up -d
